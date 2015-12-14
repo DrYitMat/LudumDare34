45dc6e00a0b0271ff99fe8d8372441a9f357594c
Wyatt Turner <wyattturner97@gmail.com>
2015-12-13 19:21:56 -0600

    Enemy Basics done, Game Pause added

--- GameTwo/main.js
+++ GameTwo/main.js
@@ -9,34 +9,125 @@ var ctx = ctx || canvas.getContext("2d");
 Melee       Ranged      Magic
 
 */
-var type = function(name){
+var Type = function(name){
   this.name = name;
   console.log(name + " Type.");
 };
 
-var enemy = function(name, type, baseHealth, baseSpeed) {
+var Enemy = function(name, type, baseHealth, baseSpeed, color) {
   this.name = name;
   this.type = type;
   this.baseHealth = baseHealth;
   this.baseSpeed = baseSpeed;
-  console.log(name + "is " + type + " type");
+  this.color = color;
+  var dimension = 50;
+  this.dimension = dimension;
+  console.log(dimension);
+  var x = canvas.width - this.dimension;
+  console.log(x);
+  this.x = x;
+  console.log(name + " is " + type.name + " type." + " Health is: " + baseHealth +
+  ", Speed is: " + baseSpeed + ".");
 };
+
+var gamePaused = false;
+
+function keyDown(e){
+  if(e.keyCode == 32){
+    pauseGame();
+  }
+}
+
+function keyUp(e){
+
+}
+
+var oneFrameTime = 1000 / 60;
+var game = window.setInterval(update, oneFrameTime);
+
+var pauseFont = {
+  size: 32,
+  font: "Mono",
+  string: "Game Paused"
+};
+var pauseString = "Game Paused";
+
+console.log(pauseFont.font);
+
+function pauseGame(){
+  if(!gamePaused){
+    window.clearInterval(game);
+    gamePaused = true;
+    console.log(pauseFont.string);
+    ctx.font = pauseFont.size.toString() + "px Mono";
+    ctx.fillText(pauseFont.string, (canvas.width / 2)
+    - ((pauseFont.string.length/2) * (pauseFont.size / 2)), 100);
+  } else if(gamePaused){
+    game = window.setInterval(update, oneFrameTime);
+    gamePaused = false;
+    console.log("Game Unpaused.");
+  }
+}
+
+var playerX = 100;
+
+var playerHealth = 5;
+var playerDimension = 50;
 
 function drawPlayer(){
   ctx.beginPath();
-  ctx.rect(100,canvas.height - 50,50,50);
+  ctx.rect(playerX ,canvas.height - 50, playerDimension, playerDimension);
   ctx.fillStyle = "#3eb48d";
   ctx.fill();
   ctx.closePath();
 }
 
 function drawEnemy(enemy){
-
+  ctx.beginPath();
+  ctx.rect(enemy.x, canvas.height - 50,enemy.dimension, enemy.dimension);
+  ctx.fillStyle = enemy.color;
+  ctx.fill();
+  ctx.closePath();
 }
+
+function updateEnemy(enemy){
+  enemy.x -= enemy.baseSpeed;
+}
+
+function enemyCollision(enemy){
+  if(enemy.x <= playerX + playerDimension){
+    console.log("Collision with " + enemy.name);
+    enemy.x = canvas.width - enemy.dimension;
+    playerHealth--;
+    checkPlayerHealth();
+    console.log("Player Health: " + playerHealth);
+  }
+}
+
+function collisionDetection(){
+  enemyCollision(testEnemy);
+}
+
+function checkPlayerHealth(){
+  if(playerHealth <= 0){
+
+  }
+}
+
+testType = new Type("Test");
+testEnemy = new Enemy("Test Enemy", testType, 100, 5,"#ff0000");
 
 function draw(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
   drawPlayer();
+  drawEnemy(testEnemy);
 }
 
-draw();
+function update(){
+  draw();
+  updateEnemy(testEnemy);
+  collisionDetection();
+}
+
+document.addEventListener("keydown", keyDown, false);
+document.addEventListener("keyup", keyUp, false);
